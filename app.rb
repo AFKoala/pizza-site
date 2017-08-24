306077f8d5c082afaa44f4408b095816722799ed
AFKoala <bubbles.murph@gmail.com>
2017-08-24 13:49:51 -0400

    confirm page

--- app.rb
+++ app.rb
@@ -5,6 +5,18 @@ get '/' do
     erb :home
 end
 
-get '/order' do
+post '/order' do
+    session[:pizSize] = params[:pizSize]
+    session[:crustType] = params[:crustType]
+    session[:sauces] = params[:sauces]
+    session[:cheese] = params[:cheese]
+    session[:meat] = params[:meat]
+    session[:veg] = params[:veg]
+    session[:deliver] = params[:deliver]
+    session[:address] = params[:address]
+    redirect '/confirm'
+end
 
+get '/confirm' do
+    erb :confirm, locals: {pizSize: session[:pizSize], crustType: session[:crustType], sauces: session[:sauces], cheese: session[:cheese], meat: session[:meat], veg: session[:veg], deliver: session[:deliver], address: session[:address]}
 end
